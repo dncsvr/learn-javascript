b394edad1a81f0b229a042d92a74a354ab55e454
cihandeniz <cihan@gcbrains.com>
2024-08-07 17:23:21 +0300

    remove gravity and add edges

--- matter-js/index.js
+++ matter-js/index.js
@@ -1,8 +1,10 @@
-const { Engine, Render, Runner, Bodies, Composite } = require("matter-js");
+const { Engine, Render, Runner, Bodies, Composite, Mouse, MouseConstraint } = require("matter-js");
 
 window.onload = function() {
-  var engine = Engine.create();
-  var render = Render.create({
+  const engine = Engine.create({
+      gravity: { x: 0, y: 0 }
+  });
+  const render = Render.create({
       canvas: document.getElementById("matter-js"),
       engine: engine,
       options: {
@@ -11,14 +13,35 @@ window.onload = function() {
       }
   });
 
-  var boxA = Bodies.rectangle(400, 200, 80, 80);
-  var boxB = Bodies.rectangle(450, 50, 80, 80);
-  var ground = Bodies.rectangle(400, 425, 810, 1, { isStatic: true });
+  const boxA = Bodies.rectangle(400, 200, 80, 80);
+  const boxB = Bodies.rectangle(450, 50, 80, 80);
+  const edges = [
+      Bodies.rectangle(0, 0, 1600, 10, { isStatic: true }),
+      Bodies.rectangle(0, 0, 10, 1200, { isStatic: true }),
+      Bodies.rectangle(0, 1190, 1600, 1200, { isStatic: true }),
+      Bodies.rectangle(1590, 0, 1600, 1200, { isStatic: true })
+  ];
 
-  Composite.add(engine.world, [boxA, boxB, ground]);
+  Composite.add(engine.world, [boxA, boxB, ...edges]);
+
+  const mouse = Mouse.create(render.canvas);
+  const mouseConstraint = MouseConstraint.create(engine, {
+    mouse: mouse,
+    constraint: {
+      // allow bodies on mouse to rotate
+      angularStiffness: 0,
+      render: { visible: false }
+    }
+  });
+
+  Composite.add(engine.world, mouseConstraint);
+
+  // keep the mouse in sync with rendering
+  render.mouse = mouse;
+
   Render.run(render);
 
-  var runner = Runner.create();
+  const runner = Runner.create();
 
   Runner.run(runner, engine);
 }
