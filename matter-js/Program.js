06408233b0bc58ef70347fbb6b6774813107d656
cihandeniz <cihan@gcbrains.com>
2024-08-19 18:40:07 +0300

    change an visual attribute through vue using animation

--- matter-js/Program.js
+++ matter-js/Program.js
@@ -4,16 +4,26 @@ const DemoWorld = require("./DemoWorld.js");
 const world = DemoWorld.new("matter-js");
 const app = createApp({
   setup() {
-    const vues = reactive([]);
-    const prefix = ref("vue");
+    const data = reactive([]);
+    const prefix = ref("#");
+    const multiplier = ref(1);
     const shown = ref(true);
 
+    function setMultiplier(newMultiplier) {
+      multiplier.value = newMultiplier;
+
+      for(let i=0; i<data.length; i++) {
+        data[i] = data[i]*multiplier.value;
+      }
+    }
+
     function add() {
-      vues.push(`#${vues.length}`)
+      console.log(multiplier);
+      data.push(data.length*multiplier.value);
     }
 
     function remove() {
-      vues.pop();
+      data.pop();
     }
 
     function toggle() {
@@ -24,9 +34,11 @@ const app = createApp({
     onUpdated(() => world.sync());
 
     return {
-      vues,
+      data,
+      multiplier,
       prefix,
       shown,
+      setMultiplier,
       add,
       remove,
       toggle
